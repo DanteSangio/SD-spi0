f9400baf0fad3d0eb4074c1e4f5db599eef676da
kevin <kevin@Kevin-Pc>
2018-09-21 17:33:29 -0300

    Implementado con Freertos, funcionando con spi

--- Example/src/SD.c
+++ Example/src/SD.c
@@ -36,73 +36,7 @@
 */
 #define DEBUGOUT(...) printf(__VA_ARGS__)
 
-#define FREC_TIMER	4	//Frec del timer es la cuarta parte del clock: 96000000/4 - NO CAMBIAR
-#define FREC_MATCH0	200	//Cant de veces que interrumpe por seg el match0
-#define FREC_MATCH1	100	//Cant de veces que interrumpe por seg el match1
-#define TICKRATE_HZ1 (FREC_TIMER*FREC_MATCH0)	// 4: cte de frec - 2 ticks por segundo (cada 500ms)
-#define TICKRATE_HZ2 (FREC_TIMER*FREC_MATCH1)	// 4: cte de frec - 1 tick por segundo	(cada 1000ms)
-
-
-//Placa Infotronic
 #define LED_STICK	PORT(0),PIN(22)
-#define	BUZZER		PORT(0),PIN(28)
-#define	SW1			PORT(2),PIN(10)
-#define SW2			PORT(0),PIN(18)
-#define	SW3			PORT(0),PIN(11)
-#define SW4			PORT(2),PIN(13)
-#define SW5			PORT(1),PIN(26)
-#define	LED1		PORT(2),PIN(0)
-#define	LED2		PORT(0),PIN(23)
-#define	LED3		PORT(0),PIN(21)
-#define	LED4		PORT(0),PIN(27)
-#define	RGBB		PORT(2),PIN(1)
-#define	RGBG		PORT(2),PIN(2)
-#define	RGBR		PORT(2),PIN(3)
-#define EXPANSION0	PORT(2),PIN(7)
-#define EXPANSION1	PORT(1),PIN(29)
-#define EXPANSION2	PORT(4),PIN(28)
-#define EXPANSION3	PORT(1),PIN(23)
-#define EXPANSION4	PORT(1),PIN(20)
-#define EXPANSION5	PORT(0),PIN(19)
-#define EXPANSION6	PORT(3),PIN(26)
-#define EXPANSION7	PORT(1),PIN(25)
-#define EXPANSION8	PORT(1),PIN(22)
-#define EXPANSION9	PORT(1),PIN(19)
-#define EXPANSION10	PORT(0),PIN(20)
-#define EXPANSION11	PORT(3),PIN(25)
-#define EXPANSION12	PORT(1),PIN(27)
-#define EXPANSION13	PORT(1),PIN(24)
-#define EXPANSION14	PORT(1),PIN(21)
-#define EXPANSION15	PORT(1),PIN(18)
-#define EXPANSION16	PORT(2),PIN(8)
-#define EXPANSION17	PORT(2),PIN(12)
-#define EXPANSION18	PORT(0),PIN(16) //RX1 No se puede usar
-#define EXPANSION19	PORT(0),PIN(15) //TX1 No se puede usar
-#define EXPANSION20	PORT(0),PIN(22) //RTS1
-#define EXPANSION21	PORT(0),PIN(17) //CTS1
-/*
-#define EXPANSION22		PORT
-#define	EXPANSION23		PORT
-#define EXPANSION24		PORT
-*/
-#define ENTRADA_ANALOG0 PORT(1),PIN(31)
-#define ENTRADA_DIG1	PORT(4),PIN(29)
-#define ENTRADA_DIG2	PORT(1),PIN(26)
-#define MOSI1			PORT(0),PIN(9)
-#define MISO1 			PORT(0),PIN(8)
-#define SCK1			PORT(0),PIN(7)
-#define SSEL1			PORT(0),PIN(6)
-
-//UART
-#define UART_SELECTION 	LPC_UART1
-#define IRQ_SELECTION 	UART1_IRQn
-#define HANDLER_NAME 	UART1_IRQHandler
-#define TXD1	0,15	//TX UART1
-#define	RXD1	0,16	//RX UART1
-
-#define UART_SRB_SIZE 32	//S:Send - Transmit ring buffer size
-#define UART_RRB_SIZE 1024	//R:Receive - Receive ring buffer size
-
 
 //SD
 #define FILE_READ         '1'
@@ -116,10 +50,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Variables globales
 
-SemaphoreHandle_t Semaforo_1;
-//SemaphoreHandle_t Semaforo_2;
 QueueHandle_t Cola_SD;
-QueueHandle_t Cola_2;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* uC_StartUp */
@@ -128,35 +59,6 @@ void uC_StartUp (void)
 	Chip_GPIO_Init (LPC_GPIO);
 	Chip_GPIO_SetDir (LPC_GPIO, LED_STICK, OUTPUT);
 	Chip_IOCON_PinMux (LPC_IOCON, LED_STICK, IOCON_MODE_INACT, IOCON_FUNC0);
-	Chip_GPIO_SetDir (LPC_GPIO, BUZZER, OUTPUT);
-	Chip_IOCON_PinMux (LPC_IOCON, BUZZER, IOCON_MODE_INACT, IOCON_FUNC0);
-	Chip_GPIO_SetDir (LPC_GPIO, RGBB, OUTPUT);
-	Chip_IOCON_PinMux (LPC_IOCON, RGBB, IOCON_MODE_INACT, IOCON_FUNC0);
-	Chip_GPIO_SetDir (LPC_GPIO, RGBG, OUTPUT);
-	Chip_IOCON_PinMux (LPC_IOCON, RGBG, IOCON_MODE_INACT, IOCON_FUNC0);
-	Chip_GPIO_SetDir (LPC_GPIO, RGBR, OUTPUT);
-	Chip_IOCON_PinMux (LPC_IOCON, RGBR, IOCON_MODE_INACT, IOCON_FUNC0);
-	Chip_GPIO_SetDir (LPC_GPIO, LED1, OUTPUT);
-	Chip_IOCON_PinMux (LPC_IOCON, LED1, IOCON_MODE_INACT, IOCON_FUNC0);
-	Chip_GPIO_SetDir (LPC_GPIO, LED2, OUTPUT);
-	Chip_IOCON_PinMux (LPC_IOCON, LED2, IOCON_MODE_INACT, IOCON_FUNC0);
-	Chip_GPIO_SetDir (LPC_GPIO, LED3, OUTPUT);
-	Chip_IOCON_PinMux (LPC_IOCON, LED3, IOCON_MODE_INACT, IOCON_FUNC0);
-	Chip_GPIO_SetDir (LPC_GPIO, LED4, OUTPUT);
-	Chip_IOCON_PinMux (LPC_IOCON, LED4, IOCON_MODE_INACT, IOCON_FUNC0);
-	Chip_GPIO_SetDir (LPC_GPIO, SW1, INPUT);
-	Chip_IOCON_PinMux (LPC_IOCON, SW1, IOCON_MODE_PULLDOWN, IOCON_FUNC0);
-
-	//Salidas apagadas
-	Chip_GPIO_SetPinOutLow(LPC_GPIO, LED_STICK);
-	Chip_GPIO_SetPinOutHigh(LPC_GPIO, BUZZER);
-	Chip_GPIO_SetPinOutLow(LPC_GPIO, RGBR);
-	Chip_GPIO_SetPinOutLow(LPC_GPIO, RGBG);
-	Chip_GPIO_SetPinOutLow(LPC_GPIO, RGBB);
-	Chip_GPIO_SetPinOutLow(LPC_GPIO, LED1);
-	Chip_GPIO_SetPinOutLow(LPC_GPIO, LED2);
-	Chip_GPIO_SetPinOutLow(LPC_GPIO, LED3);
-	Chip_GPIO_SetPinOutLow(LPC_GPIO, LED4);
 }
 
 
@@ -165,13 +67,14 @@ void uC_StartUp (void)
 static void xTaskPruebaSD(void *pvParameters)
 {
 	uint8_t i=0;
-    char str[]={"Hello World, Writing data to SD CARD using 1789"};
+    char str[]={"Hello World, Writing data to SD CARD using 1789\n\r"};
 
     for(i=0;i<4;i++)
 	{
 		xQueueSendToBack(Cola_SD, str, portMAX_DELAY);
 		vTaskDelay(1000/portTICK_RATE_MS);			//Delay 1 seg
 	}
+    vTaskDelete(NULL);	//Borra la tarea
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -212,20 +115,28 @@ static void xTaskWriteSD(void *pvParameters)
     uint8_t returnStatus,i=0;
     fileConfig_st *srcFilePtr;
     char Receive[100];
-    /* PARA ESCRIBIR ARCHIVO */
-    do
-    {
-    srcFilePtr = FILE_Open("datalog.txt",WRITE,&returnStatus);
-    }while(srcFilePtr == 0);
+
 
     while (1)
 	{
+
+        /* PARA ESCRIBIR ARCHIVO */
+        do
+        {
+        srcFilePtr = FILE_Open("datalog.txt",WRITE,&returnStatus);
+        }while(srcFilePtr == 0);
+
 		xQueueReceive(Cola_SD,&Receive,portMAX_DELAY);	//Para recibir los datos a guardar
-    	while(Receive[i])
+    	for(i=0;Receive[i];)
 		{
 		   FILE_PutCh(srcFilePtr,Receive[i++]);
 		}
         FILE_PutCh(srcFilePtr,EOF);
+        FILE_Close(srcFilePtr);
+        for(i=0;Receive[i];i++)//limpio el vector
+        {
+        	Receive[i]=0;
+        }
 	}
 	vTaskDelete(NULL);	//Borra la tarea si sale del while 1
 }
@@ -241,37 +152,8 @@ int main(void)
     SystemCoreClockUpdate();
     SystemInit();
 
-	///////////////////////////////////////////////////////////////////
-    /* PARA ESCRIBIR ARCHIVO */
-    /*    do
-    {
-    srcFilePtr = FILE_Open("datalog.txt",WRITE,&returnStatus);
-    }while(srcFilePtr == 0);
-    if(srcFilePtr == 0)
-    {
-    	DEBUGOUT("\n\rFile Opening Failed");
-    }
-    else
-    {
-    	DEBUGOUT("\n\rEscribiendo la SD..");
-    	while(str[i])
-		{
-		   FILE_PutCh(srcFilePtr,str[i++]);
-		}
-        FILE_PutCh(srcFilePtr,EOF);
-
-        FILE_Close(srcFilePtr);
-        DEBUGOUT("\n\rData saved to file, closing the file.");
-    //}*/
-
-	///////////////////////////////////////////////////////////////////
-
-
-	vSemaphoreCreateBinary(Semaforo_1);			//Creamos el semaforo
-	xSemaphoreTake(Semaforo_1, portMAX_DELAY);	//Tomamos el semaforo
 
 	Cola_SD = xQueueCreate(4, sizeof(char) * 100);	//Creamos una cola para mandar una trama completa
-	Cola_2 = xQueueCreate(1, sizeof(uint32_t));	//Creamos una cola de un elemento
 
 	//Creamos las tareas
 
@@ -279,7 +161,7 @@ int main(void)
 				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
 				(xTaskHandle *) NULL);
 	xTaskCreate(xTaskWriteSD, (char *) "xTaskWriteSD",
-				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 2UL),
+				configMINIMAL_STACK_SIZE * 2, NULL, (tskIDLE_PRIORITY + 2UL),
 				(xTaskHandle *) NULL);
 	xTaskCreate(vTaskInicSD, (char *) "vTaskInicSD",
 				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 3UL),
--- Example/src/readme.txt
+++ Example/src/readme.txt
@@ -5,3 +5,8 @@ En fat32.h cambiamos el END_OF_CLUSTER a 0xfffffff8 porque no llegaba a leer has
 El programa crea solo el archivo "DATALOG.txt", las veces que probamos creandolo no funciono.
 
 Cuando escribe arranca escribiendo desde la ultima posicion que escribio anteriormente.
+
+Al alimentar con 3V3 no detecta el formato ded la SD, DEBE ALIMENTARSE CON 5V
+
+Hasta no colocar el EOF no guarda el archivo, solución usada, se coloca el EOF tras cada "linea"
+se cierra el archivo y se vuelve a abrir nuevamente.
